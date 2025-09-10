e18698e1286ffdc8983cda9ad6b49c7f5b3356aa
Juan Parrado <jyunyauya@gmail.com>
2025-09-10 16:33:56 -0300

    Enrutador dinámico finalizado, cambios en el Home y uso de navegación.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import PaginaPrincipalPage from "./pages/Principal";
 import BuscarSabores from "./pages/BuscarSabores";
 import LoadingPage from "./pages/Loading";
+import NotFound from "./pages/NoFueEncontrado";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Redirige raíz a /home */}
+        <Route path="/" element={<Navigate to="/home" />} />
+
         <Route path="/home" element={<PaginaPrincipalPage />} />
         <Route path="/buscar" element={<BuscarSabores />} />
-        {/* Agrega más rutas aquí */}
+        <Route path="/loading" element={<LoadingPage />} />
+
+        {/* Página 404 */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/principal/Principal.jsx
+++ src/components/principal/Principal.jsx
@@ -1,23 +1,31 @@
 import "./principal.css";
+import { Link } from "react-router-dom";
 
 const Principal = () => {
   return (
-    <div>
-      <header>
-        <h1>Heladería Los Papus</h1>
-        <h3>¡Bienvenido a la mejor heladería de la ciudad!</h3>
+    <div className="principal-container">
+      <header className="principal-header">
+        <h1 className="titulo"> Heladería Los Papus</h1>
+        <h3 className="subtitulo">
+          ¡Bienvenido a la mejor heladería de la ciudad!
+        </h3>
       </header>
 
-      <section>
-        <h2>¡Un vistazo a nuestros sabores más tentadores!</h2>
-        <ul>
-          <li>Sabores</li>
-          <li>Capelinas</li>
-          <li>Bombones</li>
-        </ul>
-      </section>
+      <main className="principal-main">
+        <section className="sabores-section">
+          <h2> Un vistazo a nuestros sabores más tentadores </h2>
+          <div className="cards-container">
+            <Link to="/buscar" className="card">
+              Busqueda de Sabores
+            </Link>
 
-      <footer>
+            <div className="card"> Capelinas</div>
+            <div className="card"> Bombones</div>
+          </div>
+        </section>
+      </main>
+
+      <footer className="principal-footer">
         <p>&copy; 2025 Heladería Papus. Todos los derechos reservados.</p>
       </footer>
     </div>
