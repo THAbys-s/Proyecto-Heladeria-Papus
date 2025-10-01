5650f9927583a232ba20e77d491a5425fedbe00a
Juan Parrado <jyunyauya@gmail.com>
2025-10-01 16:37:39 -0300

    css cambiado de pagina productos

--- commands.sh
+++ commands.sh
@@ -2,10 +2,6 @@ npm install react-router-dom
 
 pip install flask-login
 
-<<<<<<< HEAD
-
-=======
->>>>>>> eb4277ae4b89e1773548968ed5dd2b8a9fb03350
 pip install pymysql
 
 npm install axios
@@ -14,9 +10,5 @@ pip install python-dotenv
 
 pip install flask-cors
 
-<<<<<<< HEAD
 pip install cryptography
 
-=======
-pip install cryptography
->>>>>>> eb4277ae4b89e1773548968ed5dd2b8a9fb03350
