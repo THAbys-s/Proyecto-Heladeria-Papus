fc9bc5845fe8825f6594b4ed44c592947282e025
Diego Ajata Ledezma <ajataledezmadiego@gmail.com>
2025-10-01 09:36:16 -0300

    Comandos para instalar de una vez por todas

---
+++ commands.sh
@@ -0,0 +1,13 @@
+npm install react-router-dom
+
+pip install flask-login
+
+pip install pymysql
+
+npm install axios
+
+pip install python-dotenv
+
+pip install flask-cors
+
+pip install cryptography
