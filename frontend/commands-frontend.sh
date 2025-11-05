f1cae3bb7adb2ccaa486597f7d9eb33ef20dfb3c
Diego Ajata Ledezma <ajataledezmadiego@gmail.com>
2025-11-05 17:28:18 -0300

    detalles

--- frontend/commands-frontend.sh
+++ frontend/commands-frontend.sh
@@ -1,24 +1,8 @@
 npm install react-router-dom
 
-pipenv install flask-login
-
-pipenv install pymysql
-
 npm install axios
 
-pipenv install python-dotenv
-
-pipenv install flask-cors
-
-pipenv install cryptography
-
 npm install react-icons
-
-pipenv install Flask pytest pytest-cov
-
-pipenv install requests faker
-
-pipenv install pytest-mock
 
 npm install --save-dev jest babel-jest @babel/preset-env @babel/preset-react
 
